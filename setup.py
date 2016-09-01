c1a864848da6535e0a0bcb03eedaeebf900c6008
Yannick Méheut <yannick@meheut.org>
2016-09-01 21:25:58 +0200

    Changed version for fix on get_netgroupmember

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
     long_description = open('README.md').read()
 
 setup(name='pywerview',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python port of PowerSploit\'s PowerView',
     long_description=long_description,
     dependency_links = ['https://github.com/CoreSecurity/impacket/tarball/master#egg=impacket-0.9.16dev'],
