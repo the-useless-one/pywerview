70bc0b3b884975e1bff44743594a93c291c1627d
ThePirateWhoSmellsOfSunflowers <ThePirateWhoSmellsOfSunflowers@users.noreply.github.com>
2021-03-02 18:12:28 +0100

    get-netgroupmember seems to be fixed

--- pywerview/functions/net.py
+++ pywerview/functions/net.py
@@ -386,25 +386,24 @@ class NetRequester(LDAPRPCRequester):
                     else:
                         final_member = adobj.ADObject(list())
 
-                    member_dn = member.distinguishedname
-                    print(member_dn)
+                    member_dn = member.distinguishedname.decode('utf-8')
                     try:
                         member_domain = member_dn[member_dn.index('DC='):].replace('DC=', '').replace(',', '.')
                     except IndexError:
                         member_domain = str()
-                    is_group = (member.samaccounttype != '805306368')
+                    is_group = (member.samaccounttype.decode('utf-8') != '805306368')
 
                     attributes = dict()
                     if queried_domain:
-                        attributes['groupdomain'] = queried_domain
+                        attributes['groupdomain'] = [queried_domain.encode('utf-8')]
                     else:
-                        attributes['groupdomain'] = self._domain
-                    attributes['groupname'] = group.name
-                    attributes['membername'] = member.samaccountname
-                    attributes['memberdomain'] = member_domain
-                    attributes['isgroup'] = is_group
-                    attributes['memberdn'] = member_dn
-                    attributes['membersid'] = member.objectsid
+                        attributes['groupdomain'] = [self._domain.encode('utf-8')]
+                    attributes['groupname'] = [group.name]
+                    attributes['membername'] = [member.samaccountname]
+                    attributes['memberdomain'] = [member_domain.encode('utf-8')]
+                    attributes['isgroup'] = [is_group]
+                    attributes['memberdn'] = [member_dn.encode('utf-8')]
+                    attributes['objectsid'] = [member.objectsid]
 
                     final_member.add_attributes(attributes)
 
