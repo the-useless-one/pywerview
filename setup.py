76c8a7a064f7d8462be21d1702481ac987a4601f
Yannick <ymeheut@almond.consulting>
2021-03-15 10:47:25 +0100

    Changed pypandoc convert function

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 try:
     import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = pypandoc.convert_file('README.md', 'rst')
 except(IOError, ImportError):
     long_description = open('README.md').read()
 
