02299693d1885f441ac778260efbb7dd0a6cb4b8
ThePirateWhoSmellsOfSunflowers <ThePirateWhoSmellsOfSunflowers@users.noreply.github.com>
2021-08-17 19:44:37 +0200

    cherry-picking commit from dev to master : cleaning module requirement
    bump master version for pip release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
     long_description = open('README.md').read()
 
 setup(name='pywerview',
-    version='0.3.1',
+    version='0.3.2',
     description='A Python port of PowerSploit\'s PowerView',
     long_description=long_description,
     dependency_links = ['https://github.com/SecureAuthCorp/impacket/tarball/master#egg=impacket-0.9.22'],
